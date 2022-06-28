2eed77821ffb3e210ea643e62631b6951c0a8b12
Mohamed Tolba <Tolbaax@gmail.com>
2022-06-28 03:14:27 +0200

    Updating

--- lib/layout/shop_layout.dart
+++ lib/layout/shop_layout.dart
@@ -1,74 +1,91 @@
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shop_app/modules/search/search_screen.dart';
 import 'package:shop_app/shared/bloc/cubit/cubit.dart';
 import 'package:shop_app/shared/bloc/cubit/states.dart';
-import 'package:shop_app/shared/components/components.dart';
+
+import '../modules/categories/categories_screen.dart';
+import '../modules/favorites/favorites_screen.dart';
+import '../modules/products/product_screen.dart';
+import '../modules/search/search_screen.dart';
+import '../modules/settings/settings_screen.dart';
+import '../shared/components/components.dart';
 
 class ShopLayout extends StatelessWidget {
+  var pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<ShopCubit, ShopStates>(
       listener: (context, state) {},
       builder: (context, state) {
-        var cubit = ShopCubit.get(context);
+        ShopCubit cubit = ShopCubit.get(context);
         return Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'Shop App',
-              ),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    navigateTo(
-                      context,
-                      SearchScreen(),
-                    );
-                  },
-                  icon: const Icon(Icons.search),
-                ),
-              ],
+          appBar: AppBar(
+            title: const Text(
+              'Shop App',
             ),
-            body: cubit.bottomScreens[cubit.currentIndex],
-            bottomNavigationBar: BottomNavyBar(
-              showElevation: true,
-              itemCornerRadius: 24,
-              selectedIndex: cubit.currentIndex,
-              curve: Curves.easeIn,
-              iconSize: 24.0,
-              onItemSelected: (int index) {
-                cubit.changeBottom(index);
-              },
-              items: <BottomNavyBarItem>[
-                BottomNavyBarItem(
-                  icon: const Icon(Icons.apps),
-                  title: const Text('Home'),
-                  activeColor: Colors.red,
-                  textAlign: TextAlign.center,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  navigateTo(
+                    context,
+                    SearchScreen(),
+                  );
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ],
+          ),
+          body: PageView(
+            controller: pageController,
+            children: const [
+              ProductsScreen(),
+              CategoriesScreen(),
+              FavoritesScreen(),
+              SettingsScreen(),
+            ],
+            onPageChanged: (newIndex) {
+              cubit.changeBottom(newIndex);
+            },
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: cubit.currentIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
                 ),
-                BottomNavyBarItem(
-                  icon: const Icon(Icons.category_outlined),
-                  title: const Text('Categories'),
-                  activeColor: Colors.purpleAccent,
-                  textAlign: TextAlign.center,
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.apps,
                 ),
-                BottomNavyBarItem(
-                  icon: const Icon(Icons.favorite_rounded),
-                  title: const Text(
-                    'Favorites',
-                  ),
-                  activeColor: Colors.pink,
-                  textAlign: TextAlign.center,
+                label: 'Categories',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
                 ),
-                BottomNavyBarItem(
-                  icon: const Icon(Icons.settings),
-                  title: const Text('Settings'),
-                  activeColor: Colors.blue,
-                  textAlign: TextAlign.center,
+                label: 'Favorites',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings,
                 ),
-              ],
-            ));
+                label: 'Settings',
+              ),
+            ],
+            onTap: (index) {
+              cubit.changeBottom(index);
+              pageController.animateToPage(
+                index,
+                duration: const Duration(microseconds: 500),
+                curve: Curves.ease,
+              );
+            },
+          ),
+        );
       },
     );
   }
